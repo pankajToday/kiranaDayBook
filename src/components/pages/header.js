236403e92c3fcf1a8841c2de11e8bdb810e4e6f5
dddd <uj@gmail.com>
2022-10-09 19:45:09 +0530

    nav top issue fixed. file compile improved.

--- src/components/pages/header.js
+++ src/components/pages/header.js
@@ -4,12 +4,12 @@ export default function header() {
   return <div>
         <header class="masthead">
             <div class="container px-4 px-lg-5 h-100">
-                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-                    <div class="col-lg-8 align-self-end">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center ">
+                    <div class="col-lg-8 align-self-end bg-linear-gradient">
                         <h1 class="text-white font-weight-bold">Your Favorite Place for buy groscery from home.</h1>
                         <hr class="divider" />
                     </div>
-                    <div class="col-lg-8 mb-3 align-self-baseline">
+                    <div class="col-lg-8 padding-1 align-self-baseline bg-linear-gradient">
                         <p class="text-white-75 mb-5">Kirana Day book not only keep track of monthly grocery product but also take care of your monthly budget.</p>
                         <a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
                     </div>
